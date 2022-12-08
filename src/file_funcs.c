72d81e93c8d348f3fcc147aea9ed5b87f2ad5441
Sergio10G <sejobmx@hotmail.es>
2022-12-08 18:13:17 +0100

    added some comments

--- src/file_funcs.c
+++ src/file_funcs.c
@@ -10,17 +10,20 @@ int	read_file(int fd, char **file_contents)
 	total_chars = 0;
 	while (read_chars != 0)
 	{
+		// Try to read BUF_SIZE bytes from the file
 		read_chars = read(fd, temp, BUF_SIZE);
 		if (read_chars == 0)
 			break;
 		if (!*file_contents)
 		{
+			// On the first lap, malloc file_contents and store read data there
 			*file_contents = malloc((read_chars) * sizeof(char));
 			if (!*file_contents)
 				return (-1);
 			ft_memcpy(*file_contents, temp, read_chars);
 		}
 		else
+			// On the following laps, malloc another array, join previously read data, free file_contents and assign new array to it
 			ft_memjoin(file_contents, total_chars, temp, read_chars);
 		total_chars += read_chars;
 	}
@@ -29,11 +32,11 @@ int	read_file(int fd, char **file_contents)
 
 void	write_contents(char *file_contents, unsigned int len)
 {
+	const char		base[16] = "0123456789abcdef";
 	unsigned int	hex_count;
 	unsigned int	i;
 	unsigned char	c;
 	char			*hex_str;
-	char			base[16] = "0123456789abcdef";
 	int				jump_flag;
 	int				repeating;
 	int				skipped;
@@ -43,6 +46,7 @@ void	write_contents(char *file_contents, unsigned int len)
 	skipped = 0;
 	while ((int)len - (int)hex_count > 0)
 	{
+		// Check for repeating lines, and skip them
 		repeating = 1;
 		i = 0;
 		while (i < 15)
@@ -69,10 +73,12 @@ void	write_contents(char *file_contents, unsigned int len)
 			skipped = 0;
 			jump_flag = 0;
 		}
+		// Print line index in hex format
 		hex_str = itohex(hex_count);
 		printf("%s%s%s    ", C_RED, hex_str, C_RESET);
 		if (hex_str)
 			free(hex_str);
+		// Print bytes in hex format
 		i = 0;
 		printf("%s", C_GREEN);
 		while (i < 16 && hex_count + i < len)
@@ -83,6 +89,7 @@ void	write_contents(char *file_contents, unsigned int len)
 				printf(" ");
 			i++;
 		}
+		// Add spaces at the end of the line if needed to align next part
 		if (i < 16)
 		{
 			int pos = (2 * i) + i / 2;
@@ -95,6 +102,7 @@ void	write_contents(char *file_contents, unsigned int len)
 		else
 			printf("    ");
 		printf("%s", C_RESET);
+		// Print bytes in char format if printable, else print dots
 		printf("%s", C_CYAN);
 		i = 0;
 		while (i < 16 && hex_count + i < len)
--- src/hexdump.c
+++ src/hexdump.c
@@ -6,12 +6,15 @@ int	main(int argc, char **argv)
 	int		fc_len;
 	char	*file_contents;
 
+	// Throw error if no arguments are provided
 	if (argc < 2)
 		error_msg("You need at least 1 parameter to run this program.\nUsage: ./hexdump [files-to-process]");
 	for (int i = 1; i < argc; i++)
 	{
+		// Init variables to avoid weird stuff, specially file_contents
 		fd = -1;
 		file_contents = 0;
+		// Print file name
 		printf("%s>> %s%s\n", C_YELLOW, argv[i], C_RESET);
 		fd = open(argv[i], O_RDWR);
 		if (fd < 0)
@@ -19,9 +22,11 @@ int	main(int argc, char **argv)
 			printf("%sCouldn't open file %s%s\n\n", C_RED, argv[i], C_RESET);
 			continue;
 		}
+		//	Read file contents and store it in file_contents
 		fc_len = read_file(fd, &file_contents);
 		if (fc_len <= 0)
 		{
+			// Error comp
 			if (fc_len == 0)
 				printf("Empty file\n\n");
 			else if (fc_len == -1)
@@ -30,6 +35,7 @@ int	main(int argc, char **argv)
 		}
 		else
 		{
+			// Write the actual hexdump and free malloc-ed data
 			write_contents(file_contents, (unsigned int)fc_len);
 			printf("\n");
 			free(file_contents);
